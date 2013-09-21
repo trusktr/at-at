ec0b6b79a59940d9254c602224dc07c87e5ace66
trusktr <trusktr@gmail.com>
2013-09-21 10:50:32 -0700

    Changed the callback from .walk() to return the complete file list instead of a single file.

--- README.md
+++ README.md
@@ -11,10 +11,22 @@ An AT-AT walker to walk all over your (filesystem) planet.
 ```javascript
 var walker = require('at-at');
 
-walker.walk('./', function(file) {
-    console.log('FILE: '+file);
+walker.walk('./', function(files) {
+    // files is an array of filenames (including the paths).
+    files.forEach(function(file) {
+        console.log(file);
+    }
 });
 ```
 
-The callback passed to walker.walk() gets called once for every file in the
-tree. The given example is similar to running the shell command `find`.
+The callback passed to walker.walk() gets called after all files have been
+traversed asynchronously. The given example is similar to running the shell
+command `find`.
+
+## Thanks to
+
+- [mscdex](github.com/mscdex/) for some insight on asynchronicity.
+- [chjj](github.com/chjj) for the initial
+  [idea](http://stackoverflow.com/questions/5827612). I wanted to make my own
+  and I learnt tons in the process.
+
--- index.js
+++ index.js
@@ -7,56 +7,72 @@ exports.walk = function() {
     return parametric.overload(arguments,
         [
             ["str", {}, function(){}],
-            ["str", {}],
             ["str", function(){}],
-            ["str"]
         ],
 
         [
             function(path, filter, callback) {
-                //TODO: filter the file list by regex. run callback on each filtered file.
+                //TODO: filter the file list by regex. run callback after walking.
             },
-            function(path, filter) {
-                //TODO: filter the file list by regex. no callback for the filtered files.
-            },
-            function(path, callback) {
-                // Run callback on each file. No filter.
+            function (path, callback) {
+                // Run callback when done walking. No filter.
+                var filelist = [];
+
                 if (! path.match(/\/$/)) {
                     path = path+'/';
                 }
+                console.log('--- path: '+path);
 
                 fs.readdir(path, function(err, files) {
+                    console.log('--- files 1: '+files+" "+files.length);
+                    var filesleft = files.length;
+
                     if (err) {
                         console.log('Error reading '+path+': '+err);
                         return false;
                     }
 
-                    if (files.length) {
-                        files.forEach(function(file) {
-                            file = path+file;
-                            //console.log('file: '+file);
-                            callback(file);
+                    if (!filesleft) {
+                        console.log('No files in '+path+'.');
+                        callback(filelist); // return empty filelist.
+                    }
+                    //files.forEach(function(file) {
+                    for (var i=0; i<files.length; i++) {
+                        (function() {
+                            var file = path + files[i];
+                            console.log('File: '+file);
                             fs.stat(file, function(err, stats) {
+                                filelist.push(file);
                                 if (err) {
                                     console.log('Error: '+err);
-                                    return false;
                                 }
+                                console.log('--- isDirectory: '+file+' '+stats.isDirectory()+' '+stats.mode);
                                 if (stats.isDirectory()) {
-                                    exports.walk(file, callback);
-                                    return true;
+                                    exports.walk(file, function(list) {
+                                        console.log('## list: '+ list+" "+ list.length);
+                                        console.log('## filelist: '+ filelist+" "+ filelist.length);
+                                        filelist = filelist.concat(list);
+                                        console.log('## filelist: '+ filelist+" "+ filelist.length);
+                                        console.log(filesleft--);
+                                        if (!filesleft) {
+                                            console.log('+++ File count: '+ filelist.length);
+                                            callback(filelist);
+                                        }
+                                    });
+                                }
+                                else { // is not a directory
+                                    console.log(filesleft--);
+                                    if (!filesleft) {
+                                        console.log('--- File count: '+ filelist.length);
+                                        callback(filelist);
+                                    }
                                 }
                             });
-                        });
-                        return true;
+                        })();
                     }
-                    else {
-                        console.log('No files in '+path+'.');
-                        return false;
-                    }
+                    //}
                 });
-            },
-            function(path) {
-                //TODO: return just the file list, but don't run callback on any files.
+
             }
         ]
     );
