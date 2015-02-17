2830586d7bf84fa7755c25bd6ffe999bbff1de8e
Joseph Orbegoso Pea <joe@trusktr.io>
2015-02-16 16:43:07 -0800

    Clean up console.logs

--- at-at.js
+++ at-at.js
@@ -21,52 +21,35 @@ exports.walk = function() {
                 if (! path.match(/\/$/)) {
                     path = path+'/';
                 }
-                //console.log('--- path: '+path);
 
                 fs.readdir(path, function(err, files) {
-                    //console.log('--- files 1: '+files+" "+files.length);
+                    if (err) throw new Error('Unable to read '+path+'.\n'+err)
+
                     var filesleft = files.length;
 
-                    if (err) {
-                        console.log('Error reading '+path+': '+err);
-                        return false;
-                    }
+                    if (!filesleft) callback(filelist); // return empty filelist.
 
-                    if (!filesleft) {
-                        console.log('No files in '+path+'.');
-                        callback(filelist); // return empty filelist.
-                    }
                     //files.forEach(function(file) {
                     for (var i=0; i<files.length; i++) {
                         (function() {
                             var file = path + files[i];
-                            //console.log('File: '+file);
                             fs.stat(file, function(err, stats) {
                                 filelist.push(file);
                                 if (err) {
                                     console.log('Error: '+err);
                                 }
-                                //console.log('--- isDirectory: '+file+' '+stats.isDirectory()+' '+stats.mode);
                                 if (stats.isDirectory()) {
                                     exports.walk(file, function(list) {
-                                        //console.log('## list: '+ list+" "+ list.length);
-                                        //console.log('## filelist: '+ filelist+" "+ filelist.length);
                                         filelist = filelist.concat(list);
-                                        //console.log('## filelist: '+ filelist+" "+ filelist.length);
-                                        //console.log(filesleft);
                                         filesleft--;
                                         if (!filesleft) {
-                                            //console.log('+++ File count: '+ filelist.length);
                                             callback(filelist);
                                         }
                                     });
                                 }
                                 else { // is not a directory
-                                    //console.log(filesleft);
                                     filesleft--;
-                                    //console.log(filesleft);
                                     if (!filesleft) {
-                                        //console.log('--- File count: '+ filelist.length);
                                         callback(filelist);
                                     }
                                 }
